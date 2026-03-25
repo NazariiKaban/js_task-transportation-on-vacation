912f4eed15e48509a0e63a973c3e4dfa4b08d833
Nazarii Fedak <nazar1fedak@gmail.com>
2026-03-25 12:42:00 +0100

    completedTask2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,7 +5,10 @@
  */
 function calculateRentalCost(days) {
   const price = 40;
+  const smallDiscount = 20;
+  const largeDiscount = 50;
   let result = 0;
+
 
   for (let i = 0; i < days; i++) {
     result += price;
@@ -14,11 +17,9 @@ function calculateRentalCost(days) {
   if (result < 120) {
     return result;
   } else if (result >= 120 && result < 280) {
-    return result - 20;
+    return result - smallDiscount;
   } else if (result >= 280) {
-    return result - 50;
-  } else {
-    return 0;
+    return result - largeDiscount;
   }
 }
 
